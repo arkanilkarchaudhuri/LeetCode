e10a8f43eb1c4c336a888704514f76bd85100603
Arkanil Karchaudhuri <84081886+arkanilkarchaudhuri@users.noreply.github.com>
2025-03-06 12:16:15 +0530

    Time: 32 ms (46.21%), Space: 14.5 MB (84.38%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isScramble(std::string s1, std::string s2) {
+        int n = s1.size();
+        if (s2.size() != n) return false;
+
+        
+        std::vector<std::vector<std::vector<bool>>> dp(n, std::vector<std::vector<bool>>(n, std::vector<bool>(n + 1, false)));
+
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j][1] = s1[i] == s2[j];
+            }
+        }
+
+       
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                for (int j = 0; j <= n - len; ++j) {
+                    for (int k = 1; k < len; ++k) {
+                       
+                        if (dp[i][j][k] && dp[i + k][j + k][len - k]) {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                       
+                        if (dp[i][j + len - k][k] && dp[i + k][j][len - k]) {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][n];
+    }
+};
