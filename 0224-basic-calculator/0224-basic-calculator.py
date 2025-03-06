9dbf59a0c2c098ffc86a092880ed17fb1ea89966
Arkanil Karchaudhuri <84081886+arkanilkarchaudhuri@users.noreply.github.com>
2025-03-06 12:38:53 +0530

    Time: 175 ms (5.82%), Space: 18.6 MB (38.46%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,7 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = f'({s.replace(" ", "")})'
+        f = lambda m: str(sum(map(int, findall(r'[+-]?\d+', m[0]))))
+        while '(' in s:
+            s = re.sub(r'\([^()]+\)', f, s).replace('--', '+')
+        return int(s)
