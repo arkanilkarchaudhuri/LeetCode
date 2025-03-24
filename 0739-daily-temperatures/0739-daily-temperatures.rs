43f13de34f2da7aace8b5e3e6af714f2d3dcb1bb
Arkanil Karchaudhuri <84081886+arkanilkarchaudhuri@users.noreply.github.com>
2025-03-24 18:44:06 +0530

    Time: 4 ms (63.02%), Space: 4 MB (81.77%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.rs
@@ -0,0 +1,23 @@
+use std::collections::VecDeque;
+impl Solution {
+    pub fn daily_temperatures(ts: Vec<i32>) -> Vec<i32> {
+        let len_ts: usize = ts.len();
+        let mut stk: VecDeque<usize> = VecDeque::with_capacity(len_ts);
+        let mut ans: Vec<i32> = vec![0; len_ts];
+        for (idx, &num) in ts.iter().enumerate() {
+            while let Some(&idx_prev) = stk.back() {
+                if ts[idx_prev] < num {
+                    ans[idx_prev] = (idx - idx_prev) as i32;
+                    stk.pop_back();
+                } else {
+                    break;
+                }
+            }
+            stk.push_back(idx);
+        }
+        while let Some(idx) = stk.pop_back() {
+            ans[idx] = 0;
+        }
+        return ans;
+    }
+}
